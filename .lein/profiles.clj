543a2dae118e7f1b6d5b4b740556de553e6263d7
Thomas Järvstrand <tjarvstrand@gmail.com>
2015-03-17 16:36:24 +0100

    Update leiningen user profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+{:user {:plugins [[cider/cider-nrepl "0.8.2" :exclusions [commons-codec]]
                   [lein-ancient      "0.6.4"]
-                  [org.clojure/tools.nrepl "0.2.7"]]
-        :dependencies [[cider/cider-nrepl "0.8.2"]
-                       [lein-ancient      "0.6.4"]
+                  [org.clojure/tools.nrepl "0.2.7" :exclusions [org.clojure/tools.namespace]]]
+        :dependencies [[org.clojure/java.classpath "0.2.0"]
+                       [org.clojure/tools.namespace "0.2.5"]
                        [org.clojure/tools.nrepl "0.2.7"]]}}
